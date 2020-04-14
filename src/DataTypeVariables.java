1dbb5adfd137896b8bc0f88da5d96807e6e8f2c1
Arquilla1 <Arquilla1@msn.com>
2020-04-14 10:32:09 -0400

    first commit

---
+++ src/DataTypeVariables.java
@@ -0,0 +1,24 @@
+public class DataTypeVariables {
+    public static void main(String[] args) {
+        boolean flag = true;
+        byte b = 6;
+        char a = 'B';
+        short index = 5;
+        int i = 95;
+        float f = 5.78f;
+        long longVar = 4576899;
+        double doubleVar = 4576899.4576899;
+
+        System.out.println("flag: " + flag);
+        System.out.println("byte: " + b);
+        System.out.println("char: " + a);
+        System.out.println("short: " + index);
+        System.out.println("integer: " + i);
+        System.out.println("float: " + f);
+        System.out.println("long: " + longVar);
+        System.out.println("double: " + doubleVar);
+
+
+    }
+}
+
